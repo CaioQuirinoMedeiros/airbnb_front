1f77c0db1e8f35b41a3b6cc0fa04289f426e16fd
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2019-07-29 08:09:09 -0300

    Adding some global styles: button and a tags queries for responsiveness toastify and rc-slider styles

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,4 +1,6 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
+import 'react-toastify/dist/ReactToastify.min.css';
+import 'rc-slider/assets/index.css';
 
 const GlobalStyle = createGlobalStyle`
   * {
@@ -10,14 +12,50 @@ const GlobalStyle = createGlobalStyle`
 
   body, html {
     background: #eee;
-    font-family: 'Helvetica Neue', 'Helvetica', Arial, sans-serif;
+    font-family: 'Roboto', 'Helvetica', Arial, sans-serif;
     text-rendering: optimizeLegibility !important;
     -webkit-font-smoothing: antialiased !important;
     height: 100%;
     width: 100%;
   }
 
-  .Toastify__toast {
+  html {
+    font-size: 62.5%;
+
+    @media only screen and (max-width: 56em) {
+      font-size: 60%;
+    }
+    @media only screen and (max-width: 37.5em) {
+      font-size: 56%;
+    }
+    @media only screen and (max-width: 25em) {
+      font-size: 50%;
+    }
+  }
+
+  button {
+    background: none;
+    outline: none;
+    border: none;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  input,
+  textarea {
+    font-family: inherit;
+    outline: none;
+  }
+
+  .custom_toast {
+    font-size: 1.6rem;
     border-radius: 5px !important;
   }
 `;
